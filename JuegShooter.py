8d3a7b90e39fe58110050ecb76a378589dcdd203
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2023-06-19 02:44:46 -0300

    PAUSA

--- JuegShooter.py
+++ JuegShooter.py
@@ -5,7 +5,6 @@ from rock_element import Rocks
 
 from pygame.locals import *
 
-
 from bullets import move_bullet,remove_bullet_off_screen,remove_bullet_when_hit_enemy,bullet_colision
 
 # Inicializamos Pygame
@@ -42,7 +41,6 @@ score = 0
 shot_sound = pygame.mixer.Sound("Sounds\shootSound.wav")
 lose_sound = pygame.mixer.Sound("Sounds\loseSound.wav")
 shot_sound.set_volume(0.1)  # Establece el volumen del sonido de disparo al 50%
-
 
 
 # PAUSA
@@ -82,21 +80,21 @@ def create_enemy():
     enemies.append(enemy)
 
 
+flag2 = False
+
 # GAME LOOP
 running = True
 clock = pygame.time.Clock()
 
-
-# INICILIAZA
-start_time = pygame.time.get_ticks()
-elapsed_time = 0
+total_paused_time = 0
 
 # CUANDO APRETE LA PAUSA EL PERSONAJE NO SE PUEDA NI MOVER NI APRETAR EL BOTON
 # CUANDOE STOY EN PAUSA NO SE LEAN LOS INPUTS DEL USUARIO
 # cUANDO ESTOY EN PAUSA NO SE LEAN LOS EVENTOS QUE NO SEAN CERRAR EL JUEGO
 
 while running:
-    # Handle events
+    current_time = pygame.time.get_ticks() /1000
+    current_time = int(current_time)
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
@@ -114,28 +112,53 @@ while running:
                 bullet_dir_x /= bullet_dir_length
                 bullet_dir_y /= bullet_dir_length
                 bullets.append((bullet_x, bullet_y, bullet_dir_x, bullet_dir_y))
+                
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 pause = not pause
 
+
+
+
 # EL TIEMPO DE PAUSA SE SUMA EN LA PROXIMA 
 
     if pause:
+        
         if flag_pause == True:
-            pause_start_time = pygame.time.get_ticks() # Guardo el el segundo en el cual el juego se pone en pausa
+            pause_start_time = pygame.time.get_ticks() /1000 # Guardo el el segundo en el cual el juego se pone en pausa
+            pause_start_time = int(pause_start_time)
             print("PRIMER SEGUNDO DE PAUSA: ",pause_start_time)
             flag_pause = False
-        paused_time = pygame.time.get_ticks() # Me quedo con el segundo en el cual el juego sale de la pausa
+        paused_time = pygame.time.get_ticks() /1000  # Me quedo con el segundo en el cual el juego sale de la pausa
+        paused_time = int(paused_time)
+        flag2 = True
         
-        print("TIEMPO EN PAUSA: ",paused_time )
-    
-    
+        print(paused_time)
+        
         
     # VERIFICAMOS LA PAUSA
     if not pause:
+    
+        # DIBUJAMOS EL CRONOMETRO
+    
+        if flag2 == True:
+            
+            primertikbandera = pygame.time.get_ticks() /1000
+            primertikbandera = int(primertikbandera)
+            total_paused_time_actual = paused_time - pause_start_time  # 5
+            total_paused_time =  total_paused_time + total_paused_time_actual # 0 + 5
+            elapsed_time =  primertikbandera - total_paused_time # 10 - 5
+            flag2 = False
         
+        
+        elapsed_time = current_time - total_paused_time
+        font = pygame.font.SysFont("microsoftjhengheimicrosoftjhengheiui", 30)
+        elapsed_time = int(elapsed_time)
+        elapsed_time_text = font.render("Time: " + str(elapsed_time ) , True, "BLACK")
+        
+        
+        print("A",total_paused_time)
         flag_pause = True
-        paused_time = 0
         
         # Movimientos del jugador
         keys = pygame.key.get_pressed()
@@ -147,8 +170,6 @@ while running:
             player.pos_y = player.caminar("up", player.pos_y)
         if keys[K_s] and player.pos_y < SCREEN_HEIGHT - PLAYER_HEIGHT:
             player.pos_y = player.caminar("down", player.pos_y)
-
-
 
 
         # LOGICA DE LAS BALAS 
@@ -165,11 +186,6 @@ while running:
         # PLAYER
         # Creamos el reactangulo del jugador
         player_rect = player.get_rect(player.pos_x,player.pos_y)
-
-
-
-
-    
 
 
         for enemy in enemies:
@@ -190,14 +206,11 @@ while running:
                 enemy.y -= player.vel - 4
         
         # Generate new enemies
-        if len(enemies) < 5:
-            create_enemy()
-            
-            
+        # if len(enemies) < 5:
+            # create_enemy()
             
             
         # DRAW STUFF
-            
             
         # Draw the game
         screen.fill("WHITE")
@@ -245,18 +258,20 @@ while running:
         score_text = font.render("Score: " + str(score), True, "BLACK")
         screen.blit(score_text, (10, 10))
         
-        # DIBUJAMOS EL CRONOMETRO
-        current_time = pygame.time.get_ticks() # Frena de reproducirse cuando estoy en pausa
-        print("Current: ",current_time )
-        elapsed_time = current_time - paused_time # Tengo que restar el tiempo de pausa a current time  
-        font = pygame.font.SysFont("microsoftjhengheimicrosoftjhengheiui", 30)
-        elapsed_time_text = font.render("Time: " + str(elapsed_time // 1000) , True, "BLACK")
         screen.blit(elapsed_time_text, (10, 40))
+    
+
+        
         
         pygame.font.get_fonts()
         pygame.display.flip()
         # Control the frame rate
+
         clock.tick(60)
         
 # Qit the game
 pygame.quit()
+
+
+# Reloj 10
+#
